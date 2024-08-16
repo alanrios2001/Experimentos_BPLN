867751c736d8f04142501bf97f2bcff572cf72de
alanr <alanrios2001@gmail.com>
2024-08-16 19:13:22 -0300

    ajustes

--- scripts/generate_data.py
+++ scripts/generate_data.py
@@ -1,26 +1,84 @@
-from openai import OpenAI
+from openai import AsyncOpenAI
 from config import settings
+from asyncio import Semaphore
 import asyncio
 
+HF_API_KEY = settings.provider.HF_API_KEY
+GROQ_API_KEY = settings.provider.GROQ_API_KEY
+OPENAI_API_KEY = settings.provider.OPENAI_API_KEY
+
+GROQ_ENDPOINT = settings.provider.GROQ_ENDPOINT
+OPENAI_ENDPOINT = settings.provider.OPENAI_ENDPOINT
+HF_ENDPOINT = settings.provider.HF_ENDPOINT
+
+USE_GROQ = settings.provider.USE_GROQ
+USE_OPENAI = settings.provider.USE_OPENAI
+USE_HF = settings.provider.USE_HF
+
+MAX_CONCURRENT_TASKS = 10
+
+
 class Provider:
-    def __init__(self, semaphore):
-        self.model_name = 'meta-llama/Meta-Llama-3-70B-Instruct'
+
+    def __init__(
+        self,
+        semaphore: Semaphore,
+        model_name: str = (
+            "llama3-8b-8192"
+            if USE_GROQ
+            else ("gpt-4o" if USE_OPENAI else "meta-llama/Meta-Llama-3.1-8B-Instruct")
+        ),
+    ):
+        self.model_name = "llama3-8b-8192"
         self.api_key = HF_API_KEY
-        self.client = AsyncInferenceClient(self.model_name, token=self.api_key)
+        self.client = AsyncOpenAI(
+            api_key=(
+                GROQ_API_KEY
+                if USE_GROQ
+                else (OPENAI_API_KEY if USE_OPENAI else HF_API_KEY)
+            ),
+            base_url=(
+                GROQ_ENDPOINT
+                if USE_GROQ
+                else (OPENAI_ENDPOINT if USE_OPENAI else HF_ENDPOINT)
+            ),
+        )
+
         self.semaphore = semaphore
 
-    async def generate_response(self, system_prompt, user_message, temperature=0.5, top_p=0.9):
+    async def generate_response(
+        self, system_prompt, user_message, temperature=0.5, top_p=0.9
+    ):
         retry_attempts = 10  # Número de tentativas de retentativa
         while retry_attempts > 0:
             try:
                 async with self.semaphore:
-                    messages = [{"role": "system", "content": system_prompt}, {"role": "user", "content": user_message}]
-                    response = await self.client.chat_completion(messages, max_tokens=2048, temperature=temperature, top_p=top_p)
-                    return response.choices[0]["message"]["content"]
+                    messages = [
+                        {"role": "system", "content": system_prompt},
+                        {"role": "user", "content": user_message},
+                    ]
+                    response = await self.client.chat.completions.create(
+                        model=self.model_name, messages=messages, max_tokens=2048, temperature=temperature, top_p=top_p
+                    )
+                    return response.choices[0].message.content
             except Exception as e:
                 if "429" in str(e) and retry_attempts > 0:
-                    print(f"Too many requests. Retrying in 60 seconds. Attempts left: {retry_attempts}")
+                    print(
+                        f"Too many requests. Retrying in 60 seconds. Attempts left: {retry_attempts}"
+                    )
                     await asyncio.sleep(60)
                     retry_attempts -= 1
                 else:
                     raise e
+
+
+if __name__ == "__main__":
+    semaphore = Semaphore(MAX_CONCURRENT_TASKS)
+    provider = Provider(semaphore)
+    response = asyncio.run(
+        provider.generate_response(
+            "Olá! Como posso te ajudar hoje?",
+            "Qual é o melhor modelo de linguagem?",
+        )
+    )
+    print(response)
--- scripts/html_to_pdf.py
+++ scripts/html_to_pdf.py
@@ -6,11 +6,11 @@ from pathlib import Path
 
 class BookExtractor:
     def __init__(self):
-        self.base_url = 'https://brasileiraspln.com/livro-pln/2a-edicao/'
+        self.base_url = "https://brasileiraspln.com/livro-pln/2a-edicao/"
         self.cap_links = []
         self.book = {}
 
-        self.output_path = Path(__file__).parents[1] / 'assets' / 'crawler'
+        self.output_path = Path(__file__).parents[1] / "assets" / "crawler"
         self.output_path.mkdir(parents=True, exist_ok=True)
 
     def run(self):
@@ -21,36 +21,36 @@ class BookExtractor:
     def get_cap_links(self):
         response = requests.get(self.base_url)
         response.raise_for_status()
-        soup = BeautifulSoup(response.content, 'html.parser', from_encoding='utf-8')
-        sidebar = soup.find('nav', {'id': 'quarto-sidebar'})
-        for item in sidebar.find_all('a', {'class': 'sidebar-link'}):
-            if 'data-bs-toggle' not in item.attrs and "/cap-" in item['href']:
-                self.cap_links.append(self.base_url + item['href'])
+        soup = BeautifulSoup(response.content, "html.parser", from_encoding="utf-8")
+        sidebar = soup.find("nav", {"id": "quarto-sidebar"})
+        for item in sidebar.find_all("a", {"class": "sidebar-link"}):
+            if "data-bs-toggle" not in item.attrs and "/cap-" in item["href"]:
+                self.cap_links.append(self.base_url + item["href"])
 
     def extract_cap_content(self):
         for link in self.cap_links:
             response = requests.get(link)
             response.raise_for_status()
-            soup = BeautifulSoup(response.content, 'html.parser', from_encoding='utf-8')
-            content = soup.find('main', {'class': 'content'})
-            chapter_titles = content.find_all('h2')
+            soup = BeautifulSoup(response.content, "html.parser", from_encoding="utf-8")
+            content = soup.find("main", {"class": "content"})
+            chapter_titles = content.find_all("h2")
             chapter_content = {}
             for title in chapter_titles:
-                if title.find('span', class_='header-section-number'):
-                    title.find('span', class_='header-section-number').decompose()
+                if title.find("span", class_="header-section-number"):
+                    title.find("span", class_="header-section-number").decompose()
                 cleaned_title = title.text.strip()
                 section_content = []
                 current = title.find_next_sibling()
-                while current and current.name != 'h2':
-                    if current.name == 'p':
+                while current and current.name != "h2":
+                    if current.name == "p":
                         section_content.append(current.text.strip())
                     current = current.find_next_sibling()
-                chapter_content[cleaned_title] = ' '.join(section_content)
+                chapter_content[cleaned_title] = " ".join(section_content)
             self.book[link.split("/")[-1].replace(".html", "")] = chapter_content
 
     def save_json(self):
-        file_path = self.output_path / 'book.json'
-        with open(file_path, 'w', encoding='utf-8') as f:
+        file_path = self.output_path / "book.json"
+        with open(file_path, "w", encoding="utf-8") as f:
             json.dump(self.book, f, ensure_ascii=False, indent=4)
 
 
